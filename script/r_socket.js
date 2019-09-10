4719a642a014013802ed73fd95afd1c6c6ac8867
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-09-10 18:24:30 +0900

    r_socket tested with URScript socket function

--- script/r_socket.js
+++ script/r_socket.js
@@ -49,6 +49,14 @@ setImmediate(async function(){
   rosNode.subscribe('/response/recipe_load',std_msgs.Bool,async function(ret){
     emitter.emit('recipe',ret.data);
   });
+  const pub_tf=rosNode.advertise('/update/config_tf',geometry_msgs.TransformStamped);
+  const pub_clear=rosNode.advertise('/request/clear',std_msgs.Bool);
+  const pub_capture=rosNode.advertise('/request/capture',std_msgs.Bool);
+  const pub_solve=rosNode.advertise('/request/solve',std_msgs.Bool);
+  const pub_recipe=rosNode.advertise('/request/recipe_load',std_msgs.String);
+  const pub_path=rosNode.advertise('/request/path',geometry_msgs.PoseArray);
+
+//Rotate J7 by J6////////////////
   if(Config.reverse_frame_id.length>0){
     let rf=Config.reverse_frame_id;
     let ctf=await rosNode.getParam('/config_tf');
@@ -67,21 +75,16 @@ setImmediate(async function(){
       });
     }
   }
-  const pub_tf=rosNode.advertise('/update/config_tf',geometry_msgs.TransformStamped);
-  const pub_clear=rosNode.advertise('/request/clear',std_msgs.Bool);
-  const pub_capture=rosNode.advertise('/request/capture',std_msgs.Bool);
-  const pub_solve=rosNode.advertise('/request/solve',std_msgs.Bool);
-  const pub_recipe=rosNode.advertise('/request/recipe_load',std_msgs.String);
-  const pub_path=rosNode.advertise('/request/path',geometry_msgs.PoseArray);
 
 //Socket events//////////////////
   const server = net.createServer(function(conn){
     conn.setTimeout(Config.socket_timeout*1000);
     let msg='';
+    let wdt=null;
     conn.on('data', function(data){
       msg+=data.toString();
-      if(msg.indexOf('(')*msg.indexOf(')')<0) return;
-console.log("rsock "+msg);
+      if(msg.indexOf('(')*msg.indexOf(')')<0) return;//until msg will like "??(???)"
+      ros.log.info("rsocket "+msg);
       if(msg.startsWith('P1') && Config.update_frame_id.length>0){
         let tf=new geometry_msgs.TransformStamped();
         tf.header.stamp=ros.Time.now();
@@ -89,16 +92,22 @@ console.log("rsock "+msg);
         tf.child_frame_id=Config.update_frame_id;
         tf.transform=protocol.decode(msg.substr(2));
         if(tf.transform!=null) pub_tf.publish(tf);
+        return;
       }
-      else if(msg.startsWith('X0')){   // [X0] ROVI_CLEAR
+      if(wdt!=null){
+        ros.log.warn("rsocket::Xcmd busy");
+        conn.write('NG\n');
+        conn.write('900');
+        return;
+      }
+      if(msg.startsWith('X0')){//--------------------[X0] ROVI_CLEAR
         let f=new std_msgs.Bool();
         f.data=true;
         pub_clear.publish(f);
         conn.write('OK\n');
       }
-      else if(msg.startsWith('X1')){   // [X1] ROVI_CAPTURE
+      else if(msg.startsWith('X1')){//--------------------[X1] ROVI_CAPTURE
         let tfs=protocol.decode(msg.substr(2).trim());
-console.log("tfs "+tfs);
         if(tfs.length>0 && Config.update_frame_id.length>0){
           let tf=new geometry_msgs.TransformStamped();
           tf.header.stamp=ros.Time.now();
@@ -106,30 +115,34 @@ console.log("tfs "+tfs);
           tf.child_frame_id=Config.update_frame_id;
           tf.transform=tfs[0];
           pub_tf.publish(tf);
-console.log("tf update "+tf);
         }
         setTimeout(function(){
           let f=new std_msgs.Bool();
           f.data=true;
           pub_capture.publish(f);
         },Config.delay);
-        let wdt=setTimeout(function(){
-            emitter.removeAllListeners('capture');
-            conn.write('NG\n');
-            conn.write('911');//timeout to capture
+        wdt=setTimeout(function(){
+          wdt=null;
+          emitter.removeAllListeners('capture');
+          ros.log.warn("rsocket::capture timeout");
+          conn.write('NG\n');
+          conn.write('911');//timeout to capture
         },Config.capt_timeout*1000);
         emitter.removeAllListeners('capture');
         emitter.once('capture',function(ret){
           clearTimeout(wdt);
-          if (ret) {
+          wdt=null;
+          ros.log.info("rsocket::capture done "+ret);
+          if(ret){
             conn.write('OK\n');
-          } else {
+          }
+          else{
             conn.write('NG\n');
             conn.write('912');//failed to capture
           }
         });
       }
-      else if(msg.startsWith('X2')){   // [X2] ROVI_SOLVE
+      else if(msg.startsWith('X2')){//--------------------[X2] ROVI_SOLVE
         let tfs=protocol.decode(msg.substr(2).trim());
         const path=new geometry_msgs.PoseArray();
         path.header.frame_id='world';
@@ -149,49 +162,60 @@ console.log("tf update "+tf);
         let f=new std_msgs.Bool();
         f.data=true;
         pub_solve.publish(f);
-        let wdt=setTimeout(function(){
-            emitter.removeAllListeners('solve');
-            conn.write('NG\n');
-            conn.write('921');//timeout to solve
+        wdt=setTimeout(function(){
+          wdt=null;
+          emitter.removeAllListeners('solve');
+          ros.log.warn("rsocket::solve timeout");
+          conn.write('NG\n');
+          conn.write('921');//timeout to solve
         },Config.solve_timeout*1000);
         emitter.removeAllListeners('solve');
         emitter.once('solve',function(ret){
           clearTimeout(wdt);
+          wdt=null;
+          ros.log.info("rsocket::solve done "+ret);
           if(!ret){
-              conn.write('NG\n');
-              conn.write('922');//failed to solve
+            conn.write('NG\n');
+            conn.write('922');//failed to solve
+            return;
           }
-          tf_lookup.stdin.write(Config.source_frame_id+' '+Config.target_frame_id+'\n');
+          tf_lookup.stdin.write(Config.base_frame_id+' '+Config.source_frame_id+' '+Config.target_frame_id+'\n');
           emitter.once('tf_transform',function(tf){
             if(tf.hasOwnProperty('translation')){
               let cod=protocol.encode([tf]);
-              ros.log.info('r_socket encode:'+cod);
+              ros.log.info("rsocket tf:"+cod);
               conn.write('OK\n');
               conn.write(cod+"\n");
             }
             else{
+              ros.log.warn("rsocket tf error");
               conn.write('NG\n');
               conn.write('923');//failed to lookup
             }
           });
         });
       }
-      else if(msg.startsWith('X3')){   // [X3] ROVI_RECIPE
+      else if(msg.startsWith('X3')){//--------------------[X3] ROVI_RECIPE
         f=new std_msgs.String();
         f.data=msg.trim().replace(/\).*/g, '').replace(/.*\(/, '');  //remove "*(" ")*"
         pub_recipe.publish(f);
         console.log("("+f.data+")");
-        let wdt=setTimeout(function(){
-            emitter.removeAllListeners('recipe');
-            conn.write('NG\n');
-            conn.write('931');//timeout to recipe
+        wdt=setTimeout(function(){
+          wdt=null;
+          emitter.removeAllListeners('recipe');
+          ros.log.warn("rsocket::recipe timeout");
+          conn.write('NG\n');
+          conn.write('931');//timeout to recipe
         },Config.recipe_timeout*1000);
         emitter.removeAllListeners('recipe');
         emitter.once('recipe',function(ret){
           clearTimeout(wdt);
-          if (ret) {
+          wdt=null;
+          ros.log.info("rsocket::recipe done "+ret);
+          if(ret){
             conn.write('OK\n');
-          } else {
+          }
+          else {
             conn.write('NG\n');
             conn.write('932');
           }
@@ -200,13 +224,19 @@ console.log("tf update "+tf);
       msg='';
     });
     conn.on('close', function(){
+      if(wdt!=null) clearTimeout(wdt);
+      wdt=null;
       ros.log.warn('r_socket CLOSED.');
     });
     conn.on('timeout',function(){
       emitter.removeAllListeners();
-      conn.write('NG\n'+"'408'"); //Request timeout
-      ros.log.error('r_socket TIMEOUT');
+      ros.log.warn('rsocket TIMEOUT');
+      conn.write("NG\n"); //Request timeout
+      conn.write("408");
       conn.destroy();
+    });
+    conn.on('error', function(err){
+      ros.log.warn('Net:socket error '+err);
     });
   }).listen(Config.port);
 
