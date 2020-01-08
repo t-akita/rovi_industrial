fd3dc05009f4cc396666d4229c832f149cdcf467
YOODS <harada@yoods.co.jp>
2020-01-08 15:40:25 +0900

    X1 with co-ordinate feature added for motoman.js

--- script/motoman.js
+++ script/motoman.js
@@ -11,7 +11,29 @@ protocol.encode=async function(tf){
   let rarr=[Math.floor(euler[3]*10000),Math.floor(euler[4]*10000),Math.floor(euler[5]*10000)]
   return tarr.concat(rarr).join(",");
 }
-
+protocol.decode=async function(msg){
+  const who=this;
+  let ary=msg.replace(/\).*/g, ']').replace(/.*\(/, '[').replace(/;/, '],[').replace(/E\+/g, 'E').replace(/\+/g, '');
+  ary=JSON.parse('['+ary+']');
+  let tfs=[];
+  for(let i=0;i<ary.length;i++){
+    let a=ary[i];
+    if(a.length>=6){
+      a[0]*=0.000001;
+      a[1]*=0.000001;
+      a[2]*=0.000001;
+      a[3]*=0.0001;
+      a[4]*=0.0001;
+      a[5]*=0.0001;
+      let tf=await who.tflib.fromEuler(a);
+      tfs.push(tf);
+    }
+    else{
+      tfs.push(a);
+    }
+  }
+  return tfs;
+}
 protocol.delim=",";
 protocol.lf="\n";
 
