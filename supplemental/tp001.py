be629cec83986ed2c032333c7443c1f21a442d78
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2020-08-13 07:15:47 +0900

    ワークとグリッパモデルを追加

---
+++ supplemental/tp001.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Python includes
+import numpy
+
+# ROS includes
+import roslib
+import rospy
+from geometry_msgs.msg import Vector3
+from tf import transformations
+
+from rviz_tools_py import rviz_tools
+
+# Initialize the ROS Node
+rospy.init_node('marker_tp001', anonymous=False, log_level=rospy.INFO, disable_signals=False)
+
+# Define exit handler
+def cleanup_node():
+    print "Shutting down node"
+    markers.deleteAllMarkers()
+
+rospy.on_shutdown(cleanup_node)
+
+markers = rviz_tools.RvizMarkers('/world', 'tp001_marker')
+
+while not rospy.is_shutdown():
+  T = transformations.translation_matrix((500,0,340))
+  scale = Vector3(1,1,1)
+  mesh_file1 = "package://rovi_industrial/mesh/TP001.stl"
+  markers.publishMesh(T, mesh_file1, 'white', scale, 0.5)
+  rospy.Rate(5).sleep() #1 Hz
