1349fe3b869aba24a11bdbafed60416d748536e6
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2020-09-14 23:57:56 +0900

    Changed address of SYSTEM function to 100-105

--- script/nachi.js
+++ script/nachi.js
@@ -7,34 +7,23 @@ const protocol=require('./protocol');
 protocol.tflib=tflib;
 
 function zeropad(N){
-    return (N.toFixed(3)+'00000').slice(10);
+  return (N.toFixed(3)+'00000').slice(10);
 }
 protocol.encode=async function(tf){
   let vecs=await this.tflib.toEuler(tf);
   let euler=vecs[0];
   return euler.map(function(N){
-        return (N.toFixed(3)+'0000000').slice(0,10);
+    return (N.toFixed(3)+'0000000').slice(0,10);
   }).join(",");
 }
 protocol.decode=async function(msg){
   let ary=protocol.decode_(msg);
   if(ary.length==0) return [];
-//  ary=ary.map(function(a){
-//    let z=a[3];
-//    a[3]=a[5];
-//    a[5]=z;
-//    return a;
-//  });
   return await this.tflib.fromEuler(ary);
 }
 protocol.jdecode=function(msg){
   let jnts=this.decode_(msg);
-  return jnts.map((jnt)=>{
-    if(jnt.length<this.joints.length) throw new Error("Joint number not enough");
-    return jnt.map(function(j){
-      return j*Math.PI/180;
-    });
-  }).slice(0,this.joints.length);
+  return jnts;
 }
 
 protocol.delim=",";
