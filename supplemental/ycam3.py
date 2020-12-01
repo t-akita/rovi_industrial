378d7ba18fe523f23a75971485aeaed5e5def405
KazukiHiraizumi <hiraizumi@yoods.co.jp>
2020-12-01 12:22:43 +0900

    撮像領域を追加

--- supplemental/ycam3.py
+++ supplemental/ycam3.py
@@ -6,9 +6,9 @@ import numpy
 # ROS includes
 import roslib
 import rospy
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3,Transform
 from tf import transformations
-
+from rovi_utils import tflib
 from rviz_tools_py import rviz_tools
 
 # Initialize the ROS Node
@@ -22,10 +22,15 @@ def cleanup_node():
 rospy.on_shutdown(cleanup_node)
 
 markers = rviz_tools.RvizMarkers('camera', 'ycam_marker')
+T1 = transformations.translation_matrix((0,0,0))
+sc1 = Vector3(1,1,1)
+tr2=Transform()
+tr2.translation.z=530
+T2=tflib.toRT(tr2)
+sc2 = Vector3(400,300,300)
 
 while not rospy.is_shutdown():
-  T = transformations.translation_matrix((0,0,0))
-  scale = Vector3(1,1,1)
   mesh_file1 = "package://rovi_industrial/mesh/YCAM3.stl"
-  markers.publishMesh(T, mesh_file1, 'grey', scale, 0.5)
-  rospy.Rate(5).sleep() #1 Hz
+  markers.publishMesh(T1,mesh_file1,'white', sc1, 0.5)
+  markers.publishCube(T2,(0.1,0.1,0.1,0.05), sc2, 0.5)
+  rospy.Rate(5).sleep() #5Hz
